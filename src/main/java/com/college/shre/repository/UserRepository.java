19c05e7a5142444b3131439b48ed14dcbdf360f0
Minkal Chaudhary <minkal.chaudhary@gupshup.io>
2025-03-20 10:16:52 +0530

    postgres CRUD

--- src/main/java/com/college/shre/repository/UserRepository.java
+++ src/main/java/com/college/shre/repository/UserRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Integer> {
     // Additional query methods can be defined here if needed
 } 
